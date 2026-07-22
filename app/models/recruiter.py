54c380f4986f37b05403e177539bcaeb63cdd6f9
padmajapatil015 <100895868+padmajapatil015@users.noreply.github.com>
2026-07-22 23:33:18 +0530

    Create recruiter.py

---
+++ app/models/recruiter.py
@@ -0,0 +1,21 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Recruiter:
+
+    agency: str
+
+    email: str
+
+    phone: str
+
+    website: str
+
+    city: str
+
+    specialization: str
+
+    hiring_location: str
+
+    verified: bool = False
