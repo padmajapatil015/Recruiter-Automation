2f0b51cfe6566d7ab11e6b16c4aea983c821bc2e
padmajapatil015 <100895868+padmajapatil015@users.noreply.github.com>
2026-07-23 09:53:18 +0530

    Update recruiter_service.py

--- app/services/recruiter_service.py
+++ app/services/recruiter_service.py
@@ -1,1 +1,23 @@
+from app.database.excel_manager import ExcelManager
 
+from app.sources.sample_source import SampleSource
+
+
+class RecruiterService:
+
+    def __init__(self):
+
+        self.database = ExcelManager()
+
+        self.source = SampleSource()
+
+
+    def run(self):
+
+        recruiters = self.source.fetch()
+
+        for recruiter in recruiters:
+
+            result = self.database.add_recruiter(recruiter)
+
+            print(result)
