641aeb77a2f6a14dfdcf3f7108fee104887c8f50
padmajapatil015 <100895868+padmajapatil015@users.noreply.github.com>
2026-07-23 09:45:21 +0530

    Update recruiter.py

--- app/models/recruiter.py
+++ app/models/recruiter.py
@@ -5,10 +5,25 @@ from dataclasses import dataclass
 class Recruiter:
 
     agency: str
+
+    recruiter_name: str
+
     email: str
+
     phone: str
+
+    linkedin: str
+
     website: str
+
     city: str
+
     specialization: str
+
+    current_opening: str
+
     hiring_location: str
+
+    source: str
+
     verified: bool = False
